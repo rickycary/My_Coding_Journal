d5345b5b1698d098a988f574d6c0a50d9c0c15b5
Richard <ricky.cary1@gmail.com>
2022-12-05 11:53:31 -0600

    Servers connected

---
+++ server.js
@@ -0,0 +1,35 @@
+// Import Dependencies
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
+// Create Express App
+const app = express();
+
+// Establish Mono Connection
+mongoose.connect(process.env.MONGO)
+
+// Mongoose Connection Events
+mongoose.connection
+.on("open", () => console.log("Connected to Mongo"))
+.on("close", () => console.log("Disconnected to Mongo"))
+.on("error", (error) => console.log(error))
+
+// Register Middleware 
+app.use(morgan("dev"));
+app.use("/static", express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+// Routes and Routers
+app.get("/", (req, res) => {
+    res.send("<h1>Server is working</h1>")
+});
+
+// Listener
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Listening onf port: ${PORT}!`)
+})
