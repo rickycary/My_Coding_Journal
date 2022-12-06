a7809f7207c2dafbda62d8fbc489f6b9e71995bc
Richard <ricky.cary1@gmail.com>
2022-12-06 14:36:29 -0600

    Index page rendered

--- controllers/journal.js
+++ controllers/journal.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Register Routes w/ the router
 router.get("/", (req, res) => {
-    res.send("You hit the journal router")
+    res.render("journal/index.ejs")
 })
 
 // Export the router
